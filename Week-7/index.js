604bb8326832451476eed1bb8670efc564c5cd42
Yaswanth Kotha <yaswanth.kotha2001@gmail.com>
2024-09-25 15:52:45 +0530

    Week-7-1 got completed

---
+++ Week-7/index.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { UserModel, TodoModel } = require("./db");
+const jwt = require("jsonwebtoken");
+const jwt_SECRET = "nothinghard";
+
+mongoose.connect(
+  "mongodb+srv://yaswanth:VSILZOxoRVP5kArL@cluster0.qaouq.mongodb.net/Todo-App"
+);
+
+const app = express();
+app.use(express.json());
+
+app.post("/signup", async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  await UserModel.create({
+    email: email,
+    password: password,
+    name: name,
+  });
+
+  res.json({
+    message: "You are signed up",
+  });
+});
+
+app.post("/signin", async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await UserModel.findOne({
+    email: email,
+    password: password,
+  });
+
+  console.log(user);
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: user._id.toString(),
+      },
+      jwt_SECRET
+    );
+
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "lincorrect credentials",
+    });
+  }
+});
+
+function auth(req, res, next) {
+  const token = req.headers.token;
+
+  const decodedData = jwt.verify(token, jwt_SECRET);
+
+  if (decodedData) {
+    req.userId = decodedData.id;
+    next();
+  } else {
+    res.status(403).json({
+      message: "Incorrent credentials",
+    });
+  }
+}
+
+app.post("/todo", auth, async function (req, res) {
+  const userId = req.userId;
+  const title = req.body.title;
+  const done = req.body.done;
+
+  await TodoModel.create({
+    title: title,
+    done: done,
+    userId: userId,
+  });
+
+  res.json({
+    message: "Todo Created",
+  });
+});
+
+app.get("/todos", auth, async function (req, res) {
+  const userId = req.userId;
+
+  const todos = await TodoModel.find({
+    userId: userId,
+  });
+
+  console.log(todos);
+
+  res.json({
+    todos,
+  });
+});
+
+app.listen(3000);
